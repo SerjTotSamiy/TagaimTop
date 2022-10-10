2dbacdfe66c13e8a82e4efa1f279837f808232c2
artvezhl <artvezhl@gmail.com>
2022-10-10 23:56:25 +0600

    fix GTM

--- pages/_document.js
+++ pages/_document.js
@@ -23,25 +23,34 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
           />
-          <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-PHCMNFB');`,
-              }}
-          />
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-RDBNGEZT97"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-RDBNGEZT97');
+            </script>
+          {/*<script*/}
+          {/*    dangerouslySetInnerHTML={{*/}
+          {/*      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':*/}
+          {/*              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],*/}
+          {/*              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=*/}
+          {/*              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);*/}
+          {/*              })(window,document,'script','dataLayer','GTM-PHCMNFB');`,*/}
+          {/*    }}*/}
+          {/*/>*/}
         </Head>
         <body>
           <Main />
           <NextScript />
-          <noscript
-              dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHCMNFB"
-                              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-              }}
-          />
+          {/*<noscript*/}
+          {/*    dangerouslySetInnerHTML={{*/}
+          {/*      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHCMNFB"*/}
+          {/*                    height="0" width="0" style="display:none;visibility:hidden"></iframe>`,*/}
+          {/*    }}*/}
+          {/*/>*/}
         </body>
       </Html>
     );

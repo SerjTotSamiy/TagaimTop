9cef3369ac609735e60718839b70898699710a56
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-10-11 23:54:43 +0300

    test

--- shared/gaEvents.js
+++ shared/gaEvents.js
@@ -1,11 +1,13 @@
 import ReactGA from "react-ga4";
 
-const successful_payment = () => ReactGA.event("payment_succesful",{methods:"payment_succesful"})
+const successful_payment = () => ReactGA.event("payment_successful",{methods:"payment_successful"})
 const fail_payment = () => ReactGA.event("payment_failed",{methods:"payment_failed"})
+const clickOnButton = () => ReactGA.event("clickOnButton",{methods:"clickOnButton"})
 
 const gaEvent = {
 	successful: successful_payment,
-	failed: fail_payment
+	failed: fail_payment,
+	buttonClick: clickOnButton
 }
 
 export {gaEvent};
